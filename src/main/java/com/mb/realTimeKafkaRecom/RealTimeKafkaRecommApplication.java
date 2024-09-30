62000cfd5b59a261b03b960b20dd42e33ec1394e
satyam.sonker <satyam.sonker@magicbricks.com>
2024-10-01 00:39:16 +0530

    first commit

---
+++ src/main/java/com/mb/realTimeKafkaRecom/RealTimeKafkaRecommApplication.java
@@ -0,0 +1,13 @@
+package com.mb.realTimeKafkaRecom;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RealTimeKafkaRecommApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RealTimeKafkaRecommApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mb/realTimeKafkaRecom/RealTimeKafkaRecommApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mb.realTimeKafkaRecom;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RealTimeKafkaRecommApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
